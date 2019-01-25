dabe4f5f805b10ba86df1a39044cc8c1adf529c2
archey347 <archey.barrell@outlook.com>
2019-01-25 23:28:33 +0000

    Added sitemap and robots.txt base url

--- routes/routes.php
+++ routes/routes.php
@@ -6,4 +6,11 @@ $app->redirect("/about", "site/index.html", 301);
 $app->redirect("/legal", "site/index.html", 301);
 $app->redirect("/privacy", "site/index.html", 301);
 
+foreach(["/sitemap.xml", "/robots.txt"] as $url) {
+    $app->get($url, function ($request, $response, $args) {
+        $args['fromRoot'] = true;
+        return UserFrosting\Sprinkle\WAdapter\Controller\Controller::handlePage($request, $response, $args);
+    });
+}
+
 $app->get("/site/{params:.*}", "UserFrosting\Sprinkle\WAdapter\Controller\Controller:handlePage");
